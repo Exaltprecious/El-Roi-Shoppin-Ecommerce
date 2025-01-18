f518bb505bc18e986aee24491251c66892c2546a
Exaltprecious <peculiarchristj1234@gmail.com>
2025-01-18 08:48:31 +0100

    An index.js file to commit

---
+++ src/middleware/index.js
@@ -0,0 +1,12 @@
+import { checkNameForCategory } from "./category.js";
+import { validateProductData } from "./product.js";
+import { checkRoles } from "./user.js";
+import { verifyToken, isAdmin } from "./authjwt.js";
+
+export {
+	checkNameForCategory,
+    validateProductData,
+    checkRoles,
+    verifyToken,
+    isAdmin,
+};
