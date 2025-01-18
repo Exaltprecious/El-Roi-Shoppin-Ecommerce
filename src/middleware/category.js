e3aab0219603e6c4f4a9de14c7b29b5f4181a0f9
Exaltprecious <peculiarchristj1234@gmail.com>
2025-01-18 08:44:33 +0100

    Add category.js

---
+++ src/middleware/category.js
@@ -0,0 +1,9 @@
+export async function checkNameForCategory(req,res,next){
+	const categoryData = req.body;
+
+	if(!categoryData.name){
+		res.status(400).send({msg: 'name is mandatory'})
+		return;
+	}
+	next()
+}
